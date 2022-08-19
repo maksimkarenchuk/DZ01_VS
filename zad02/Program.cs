a44b7205f488fe18769fb1a3f10e3f685e8ff575
max <mail.com>
2022-08-19 23:22:58 +0300

    Найти максимальное чило из трех

---
+++ zad02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");  
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (a > b && a > c) max = a;
+else if (b > a && b > c) max = b;
+else if (c > a && c > b) max = c;
+Console.WriteLine("Максимальное число {0}", max );
